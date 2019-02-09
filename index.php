8f8327b08ca46164a6acd7ba718f19f5a3dec4c2
muirne <muirne22@hotmail.com>
2019-02-09 23:29:07 +0100

    compleate full-size page

--- index.php
+++ index.php
@@ -1,30 +1,45 @@
 <?php get_header(); ?>
 <div class="wrapper">
-  <div class="container-fluid four">
-      <div class="row">
+
+
+<div class="container-fluid main">
+  <div class="container four">
+      <div class="row offer">
           <div class="col-md-8">
-              <p>Nasze produkty:</p>
+              <div class="col-md-12 offer__colone">
+                  <p class="offer__col--products">Nasze produkty:</p>
+              </div>
           </div>
               <div class="col-md-4">
-                <p>Znajdź nas na FB >></p>
+                  <div class="col-md-12 offer__coltwo">
+                     <p class="offer__col--find">Znajdź nas na FB >></p>
+                  </div>
               </div>
       </div>
-
 </div>
-  <div class="container-fluid five">
+<div class="container-fluid ghost">
+        <div class="row">
+            <div class="col-md-6 ghost__grey">
+            </div>
+            <div class="col-md-6">
+            </div>
+        </div>
+</div>
+</div>
+
+  <div class="container five">
                <?php
                  $q1 = new WP_Query([
                                    'post_type' => 'post',
                                    'posts_per_page' => 3,
                                    'category_name' => 'kabiny'
-
                            ]);
                ?>
-     <div class="row row-flex">
+     <div class="row">
          <?php if ( $q1->have_posts() ) : while ( $q1->have_posts() ) :    $q1->the_post(); ?>
                               <!-- post -->
 
-            <div class="col-md-4 col-sm-6 col-xs-12 d-flex align-items-stretch">
+            <div class="col-md-4 col-sm-6 col-xs-12">
                <div id="bod" class="card card-body flex-fill">
                 <div class="card-img-top img-responsive"><?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?></div>
 
@@ -32,7 +47,7 @@
                      <div class="card-block">
                         <h2 id="title" class="card-title"><?php the_title(); ?></h2>
                         <div class="card-text"><?php the_excerpt(); ?></div>
-                        
+
                     </div>
                 </div>
             </div>
@@ -44,6 +59,7 @@
                           <?php endif; ?>
 
           </div>
+
     </div>
     </div>
 <?php get_footer(); ?>
